d94da0dc74dfa1e8bccdb8d4f409a366f3ab660e
S ARPITHA SHETTY <sarpithashetty442@gmail.com>
2025-10-03 10:08:10 +0530

    Add files via upload

---
+++ pro2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Animal
+{
+    protected void Makesound()
+    {
+        Console.WriteLine("animal");
+    }
+class Dog : animal
+    {
+        public void bark()
+        {
+            Makesound();
+            Console.WriteLine("dog is barking");        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Dog d = new Dog();
+        d.barking();
+    }
+}
+    
---
+++ pro3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Animal
+{
+    protected void MakeSound()
+    {
+        Console.WriteLine("animal");
+
+    }
+}
+class Dog : Animal
+{
+    public void bark()
+    {
+        MakeSound();
+        Console.WriteLine("dog is barking");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Dog d = new Dog();
+    }
+}
