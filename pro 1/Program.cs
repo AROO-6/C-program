f54b63722206bc856d2cd829a2cfde7cb6b05493
S ARPITHA SHETTY <sarpithashetty442@gmail.com>
2025-10-03 10:05:52 +0530

    init commit

---
+++ pro 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Myclass
+{
+    private int age;
+    public void ShowAge()
+    {
+        age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("my age is" +age);
+        
+        
+    }
+}
+class Program
+    {
+        static void Main()
+        {
+            Myclass a = new Myclass();
+            a.ShowAge();
+        }
+    }
+
+         
