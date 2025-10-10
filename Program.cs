1063f7682b5322364fbd90daa76231f8e51775a5
S ARPITHA SHETTY <sarpithashetty442@gmail.com>
2025-10-09 18:01:47 -1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+delegate void greet();
+class program
+{
+    static void method1()
+    {
+        Console.WriteLine("hi");
+    }
+    static void method2()
+    {
+        Console.WriteLine("hello");
+    }
+    static void Main()
+    {
+        greet g = method1;
+        g += method2;
+        g();
+    }
+}
